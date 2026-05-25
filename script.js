acbda2419a5a93a95307d14cc07c470d614b4028
Maltheholle <maltheholle04@gmail.com>
2026-05-25 23:29:05 +0200

    Tilføjede JS, som sørger for at den beskrivende tekst også ændres

--- script.js
+++ script.js
@@ -5,16 +5,20 @@
 const circles = document.querySelectorAll(".circle");
 const speaker = document.querySelector(".color-profile");
 
+const title = document.querySelector("#color-description-headline");
+const description = document.querySelector("#color-description");
+
+
 circles.forEach(circle => {
 
     circle.addEventListener("click", () => {
 
-        // Fjerner aktiv styling
+
         circles.forEach(c => {
             c.classList.remove("active");
         });
 
-        // Tilføjer aktiv styling
+
         circle.classList.add("active");
 
 
@@ -23,11 +27,23 @@ circles.forEach(circle => {
 
             speaker.src = "body.img/sage-green-profile-pic.svg";
 
+        title.textContent = "SAGE GREEN";
+
+        description.innerHTML =
+        `Nice, natural and <br>
+        minimalistic beauty.`;
+
         }
 
         else if(circle.classList.contains("lavender")){
 
             speaker.src = "body.img/lavender-profile-pic.svg";
+
+            title.textContent = "LAVENDER";
+
+        description.innerHTML =
+        `Elegant, soft and <br>
+        pure vibes.`;
 
         }
 
@@ -35,11 +51,21 @@ circles.forEach(circle => {
 
             speaker.src = "body.img/warm-taupe-profile-pic.svg";
 
+
+             title.textContent = "WARM TAUPE";
+
+        description.innerHTML =
+        `Grounded, warm and <br>
+        timeless design.`;
         }
 
         else{
 
             speaker.src = "body.img/dusty-rose-profile-pic.svg";
+
+        description.innerHTML =
+            `Warm, cozy and <br>
+            perfect for the sunset.`;
 
         }
 
