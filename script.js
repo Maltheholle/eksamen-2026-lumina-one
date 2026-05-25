4f10f3e57ee0bd9ad4c5a6cf8611f71e59b59a70
Maltheholle <maltheholle04@gmail.com>
2026-05-25 23:15:50 +0200

    Tilføjer første runde js
    Nu kan man ved at trykke på de farvede cirkler, skifte højtalerens farve

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 "use strict"
+
+
+
+const circles = document.querySelectorAll(".circle");
+const speaker = document.querySelector(".color-profile");
+
+circles.forEach(circle => {
+
+    circle.addEventListener("click", () => {
+
+        // Fjerner aktiv styling
+        circles.forEach(c => {
+            c.classList.remove("active");
+        });
+
+        // Tilføjer aktiv styling
+        circle.classList.add("active");
+
+
+        // Skifter billede
+        if(circle.classList.contains("sage")){
+
+            speaker.src = "body.img/sage-green-profile-pic.svg";
+
+        }
+
+        else if(circle.classList.contains("lavender")){
+
+            speaker.src = "body.img/lavender-profile-pic.svg";
+
+        }
+
+        else if(circle.classList.contains("taupe")){
+
+            speaker.src = "body.img/warm-taupe-profile-pic.svg";
+
+        }
+
+        else{
+
+            speaker.src = "body.img/dusty-rose-profile-pic.svg";
+
+        }
+
+    });
+
+});
